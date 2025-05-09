8e959675b7e19d9f428b41a187834a9d161f65e5
Ruy Junior <ruyjunior21@gmail.com>
2025-05-09 07:52:42 -0300

    Atualização de preços

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -3,21 +3,21 @@ import { IPricing } from "@/types";
 export const tiers: IPricing[] = [
     {
         name: 'Basic',
-        price: 129,
+        price: 139,
         features: [
             '2 weekly classes',
         ],
     },
     {
         name: 'Plus',
-        price: 149,
+        price: 159,
         features: [
             '3 weekly classes',
         ],
     },
     {
         name: 'Gold',
-        price: 169,
+        price: 189,
         features: [
             '5 weekly classes',
         ],
